63a3a4eae5b1fa52659690f6a7db57abc10de694
haniyeshnz <shahnavazihaniye@gmail.com>
2023-10-30 14:53:08 +0330

    end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Logo from "./Component/Logo";
+import Form from "./Component/Form";
+import PackingList from "./Component/PackingList";
+import Stats from "./Component/Stats";
 
-function App() {
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItem(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handelToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+  function handelClearItems(){
+    const confirmed=window.confirm("Are you sure you want to delete all items?")
+   if(confirmed) setItems([])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form onHandleAddItem={handleAddItem} />
+      <PackingList
+        items={items}
+        OnDeleteItem={handleDeleteItem}
+        onToggleItem={handelToggleItem}
+        onClearItems={handelClearItems}
+      />
+      <Stats items={items} />
     </div>
   );
 }
-
-export default App;
